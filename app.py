cfe503e568b8b5db2999aee233c41401b4e8c161
tutarbu <patibotr@gmail.com>
2025-04-05 14:05:18 +0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, request, jsonify
+import subprocess
+
+app = Flask(__name__)
+
+@app.route("/get_link")
+def get_link():
+    yt_url = request.args.get("yt")
+    if not yt_url:
+        return jsonify({"error": "YouTube link eksik"}), 400
+    try:
+        result = subprocess.check_output(["yt-dlp", "-f", "best", "-g", yt_url])
+        return jsonify({"video_url": result.decode().strip()})
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
